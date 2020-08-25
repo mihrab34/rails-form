0f45b31dcde0777893ff736d48bd9e7d399dfd93
Akintola Ramota <akintolarahmah@gmail.com>
2020-08-25 20:56:06 +0000

    added model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+  validates :email, presence :true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, presence :true
 end
