a997eaaea6d549ddd8741dba4568981ba8ee2134
Akintola Ramota <akintolarahmah@gmail.com>
2020-08-26 01:24:13 +0000

    Linter error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -18,12 +17,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
 
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 end
-
