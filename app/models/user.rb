b4d78ed7506958d434197fb4e86566b0f5154737
Akintola Ramota <akintolarahmah@gmail.com>
2020-08-27 21:01:20 +0000

    fixed linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validates :email, presence: true,
-  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email, presence: true
   validates :password, presence: true
 end
