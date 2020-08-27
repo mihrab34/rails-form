11d464202e8a7437b0a88cd82f07bbfa4249b9bf
Akintola Ramota <akintolarahmah@gmail.com>
2020-08-27 17:29:11 +0000

    added update form method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email],password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -16,6 +15,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user= User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validates :email, presence: true
-  # format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email, presence: true,
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+
+    [a-z]{2,})\z/i
+  }
   validates :password, presence: true
 end
